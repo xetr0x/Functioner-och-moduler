3c0438b3dd68174ee46ec99c5d4d24f152c8ab32
kevin wilusz <kwilusz10@gmail.com>
2022-01-13 15:07:56 +0100

    keep trying to do 4.1

---
+++ one.js
@@ -0,0 +1,45 @@
+'use strict'
+
+window.onload = () => {
+const one = document.createElement("h2");
+one.textContent = "One";
+one.style = "text-align:center;";
+
+
+const text = document.createElement("h2");
+text.textContent = "bilmärken";
+text.style="margin-left:10%; margin-top:20px;"
+
+const textbox1 = document.createElement("input");
+textbox1.style = "margin-left:10%;";
+
+const info = document.createElement("textarea");
+info.style = "float:right; margin-right:65%;"
+
+const knapp = document.createElement("button");
+
+knapp.style = "margin-left:10%; margin-top:15px;"
+knapp.innerHTML = "Add Data";
+
+document.getElementById("one").appendChild(one);
+document.getElementById("one").appendChild(text);
+document.getElementById("one").appendChild(textbox1);
+document.getElementById("one").appendChild(info);
+document.getElementById("one").appendChild(knapp);
+
+
+}
+
+let carArray = [];
+
+knapp.onclick = () => 
+{
+        carArray.push(textbox1.value);
+    }
+
+
+
+
+function Unik(value, index, self){
+    return self.indexOf(value) === index;
+}
